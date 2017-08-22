7bf381641fc16893e026ea5ba026cc86b99ecbf2
arisettia <31029147+arisettia@users.noreply.github.com>
2017-08-22 11:29:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,16 @@ var config = {
 };
 
 var pool = new Pool(config);
-
-app.get('/test-db', function(req, res) {
+app.get('/test-db', function (req, res) {
     /// make a select
     //return response
-   
+    pool.query('SELECT * FROM test', function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JASON.stringify(result))
+        }
+    });
 });
 
 var app = express();
